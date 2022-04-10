cbfceaeaf86e82ab6901c82e484d42b6313d1680
Hubert <36489757+ZerolBozi@users.noreply.github.com>
2022-04-10 20:13:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,19 @@ from linebot import (
 )
 from flask_sqlalchemy import SQLAlchemy
 
+# set server and database coning
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://name:pw@address/database'
 db = SQLAlchemy(app)
 
+# set line bot config
 line_bot_api = LineBotApi('token')
 handler = WebhookHandler('secret')
 
 if __name__ == "__main__":
     from handle import *
-    app.run(debug=True,port=5002)
+    '''
+    start server
+    import handle to set route and callback
+    '''
+    app.run(host='0.0.0.0',debug=True,port=5002)
